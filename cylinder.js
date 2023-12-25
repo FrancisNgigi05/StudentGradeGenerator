b245f19db2c8f7d21a07c8a6ada8dcc61bcf18bc
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2023-12-25 09:57:11 +0300

    completed assignment

--- cylinder.js
+++ cylinder.js
@@ -1,13 +1,21 @@
+/**
+ * Creates a new Cylinder object with the specified radius and height.
+ * @param {number} radius - The radius of the cylinder.
+ * @param {number} height - The height of the cylinder.
+ */
 function Cylinder(radius, height) {
     this.radius = radius;
     this.height = height;
 
-    this.cylinderVolume = () => {
+    /**
+     * Calculates and returns the volume of the cylinder.
+     * @returns {number} The volume of the cylinder.
+     */
+    this.cylinderVolume = function() {
         const volumeOfCylinder = Math.PI * Math.pow(this.radius, 2) * this.height;
-        return (volumeOfCylinder.toFixed(3));
+        return volumeOfCylinder.toFixed(3);
     }
 }
-
 const cylinder1 = new Cylinder(12, 27);
 
 console.log(`Volume of the cylinder is ${cylinder1.cylinderVolume()}`);
